8a9c71b81ae31cdb24f146dddcfc8faab56db4f2
JinminZZang <yangjinmin95@gmail.com>
2022-03-28 19:00:00 +0900

    news-viewer ing...

---
+++ news-viewer/src/App.js
@@ -0,0 +1,45 @@
+/*
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=480574c86c5e4582b8c7e6c78a848588');
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} />}
+    </div>
+  );
+};
+
+export default App;
+*/
+
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
+import { useCallback, useState } from 'react';
+
+const App = () => {
+  const [category, setCategoty] = useState('all');
+  const onSelect = useCallback(category => setCategoty(category), []);
+
+  return (
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList categoty={category} />
+    </>
+  );
+};
+
+export default App;
