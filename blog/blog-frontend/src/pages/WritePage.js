8bef1e3f0907eae42d65056d7c7fa759321622ed
JinminZZang <yangjinmin95@gmail.com>
2022-04-12 16:08:35 +0900

    글쓰기 기능 구현하기

--- blog/blog-frontend/src/pages/WritePage.js
+++ blog/blog-frontend/src/pages/WritePage.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Responsive from '../components/common/Responsive';
+import Editor from '../components/write/Editor';
+import TagBox from '../components/write/TagBox';
+import WriteActionButtons from '../components/write/WriteActionButtons';
+import EditorContainer from '../containers/write/EditorContainer';
+import TagBoxContainer from '../containers/write/TagBoxContainer';
+import WriteActionButtonsContainer from '../containers/write/WriteActionButtonsContainer';
 
 const WritePage = () => {
-  return <div>글쓰기</div>;
+  return (
+    <Responsive>
+      {/*<Editor/>*/}
+      <EditorContainer/>
+      {/*<TagBox/>*/}
+      <TagBoxContainer/>
+      {/*<WriteActionButtons/>*/}
+      <WriteActionButtonsContainer/>
+    </Responsive>
+  );
 };
 
 export default WritePage;
