bb49240f4f0991e9523f6ac341b3b96419addbfc
JinminZZang <yangjinmin95@gmail.com>
2022-06-13 16:31:02 +0900

    metamask-test

--- metamask-test/src/App.js
+++ metamask-test/src/App.js
@@ -34,25 +34,7 @@ function App() {
         }
     }
 
-    /*    const transactionParameters = {
-            nonce: '0x00', // ignored by MetaMask
-            gasPrice: '0x09184e72a000', // customizable by user during MetaMask confirmation.
-            gas: '0x2710', // customizable by user during MetaMask confirmation.
-            to: '0x0000000000000000000000000000000000000000', // Required except during contract publications.
-            from: window.ethereum.selectedAddress, // must match user's active address.
-            value: '0x00', // Only required to send ether to the recipient from the initiating external account.
-            data:
-                '0x7f7465737432000000000000000000000000000000000000000000000000000000600057', // Optional, but used for defining smart contract creation and interaction.
-            chainId: '0x3', // Used to prevent transaction reuse across blockchains. Auto-filled by MetaMask.
-        };
-
-        // txHash is a hex string
-        // As with any RPC call, it may throw an error
-        const txHash = window.ethereum.request({
-            method: 'eth_sendTransaction',
-            params: [transactionParameters],
-        });*/
-
+    //Sending Ethereum to an address
     async function SendEth() {
         window.ethereum
             .request({
@@ -84,3 +66,7 @@ function App() {
 }
 
 export default App;
+
+/*
+* ref : https://www.youtube.com/watch?v=0nNJzq8k0h8
+*/
